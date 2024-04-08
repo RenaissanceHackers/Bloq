1255e0989f54928a4dcd656f6c3084d16a472ecc
Amateur <2863217986@qq.com>
2024-04-08 14:03:54 +0800

    create different card name

--- mobile/assets/data/taskcard.ts
+++ mobile/assets/data/taskcard.ts
@@ -3,7 +3,7 @@ const TestTaskCard = [
         id: "3",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F014dbccce1d64385a3b2bba443692d87&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Blue Phoenix",
         assignment: "Write a concise analysis on the emergence and impact of decentralized finance (DeFi) platforms.",
         point: 1000,
         token: 1000,
@@ -13,7 +13,7 @@ const TestTaskCard = [
         id: "7",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F67a581d70ad44bfeb27847855a892d25&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Red Tiger",
         assignment: "Research the vulnerabilities and best practices for ensuring security in smart contracts.",
         point: 1000,
         token: 1000,
@@ -23,7 +23,7 @@ const TestTaskCard = [
         id: "8",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252Fcd63bcb78bde46279789ca1aba495691&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Green Lion",
         assignment: "Analyze the tokenomics model of a DeFi project, including its distribution and utility.",
         point: 1000,
         token: 1000,
@@ -33,7 +33,7 @@ const TestTaskCard = [
         id: "9",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F02f1e84bba9c4d0ea4e4e43a40c854e1&w=1920&q=7',
-        name: "Renzo Protocol",
+        name: "Yellow Eagle",
         assignment: "Research and compare layer 2 scaling solutions for blockchain networks.",
         point: 1000,
         token: 1000,
@@ -43,7 +43,7 @@ const TestTaskCard = [
         id: "11",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F67a581d70ad44bfeb27847855a892d25&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Orange Bear",
         assignment: "Explore cross-chain swaps and their role in facilitating interoperability.",
         point: 1000,
 
@@ -55,7 +55,7 @@ const TestTaskCard = [
         id: "16",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F1874ed15459645e7b8f3afb21a38041f&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Purple Wolf",
         assignment: "Review privacy-preserving technologies in blockchain networks.",
         point: 1000,
 
@@ -65,7 +65,7 @@ const TestTaskCard = [
         id: "17",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F3b01496c77da4ce99a4e855eb48ee77a&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Pink Serpent",
         assignment: "Investigate decentralized identity systems built on blockchain technology.",
         point: 1000,
 
@@ -75,7 +75,7 @@ const TestTaskCard = [
         id: "19",
         image:
             'https://solana.com/_next/image?url=%2F_next%2Fstat…%2Fmedia%2Fhero-geometry.e6d9398b.png&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Black Falcon",
         assignment: "Write a short report on the challenges and solutions for achieving interoperability between blockchain networks.",
         point: 1000,
 
--- mobile/assets/data/taskcard2.ts
+++ mobile/assets/data/taskcard2.ts
@@ -4,7 +4,7 @@ const TestTaskCard2 = [
         id: "8",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252Fcd63bcb78bde46279789ca1aba495691&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "Yellow Eagle",
         assignment: "Analyze the tokenomics model of a DeFi project, including its distribution and utility.",
         point: 1000,
         token: 1000,
@@ -14,7 +14,7 @@ const TestTaskCard2 = [
         id: "9",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F02f1e84bba9c4d0ea4e4e43a40c854e1&w=1920&q=7',
-        name: "Renzo Protocol",
+        name: "Pink Serpent",
         assignment: "Research and compare layer 2 scaling solutions for blockchain networks.",
         point: 1000,
         token: 1000,
@@ -24,7 +24,7 @@ const TestTaskCard2 = [
         id: "11",
         image:
             'https://solana.com/_next/image?url=https%3A%2F%2Fcdn.builder.io%2Fapi%2Fv1%2Fimage%2Fassets%252Fce0c7323a97a4d91bd0baa7490ec9139%252F67a581d70ad44bfeb27847855a892d25&w=1920&q=75',
-        name: "Renzo Protocol",
+        name: "White Dragon",
         assignment: "Explore cross-chain swaps and their role in facilitating interoperability.",
         point: 1000,
 
